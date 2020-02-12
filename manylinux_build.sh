e6335be5005fc1515c6a7cfdac94b8be2763bc83
Will Ayd <william.ayd@icloud.com>
2020-02-12 11:10:01 -0800

    removed install / testing

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -14,7 +14,7 @@ fi
 PYLOC=/opt/python/cp${PYVER2}-cp${PYVER2}${ABIVER}
 
 ${PYLOC}/bin/python -m pip install --upgrade pip setuptools wheel auditwheel
-${PYLOC}/bin/python -m pip install cython numpy pytz python-dateutil
+${PYLOC}/bin/python -m pip install cython numpy
 
 cd /io
 ${PYLOC}/bin/python setup.py bdist_wheel
