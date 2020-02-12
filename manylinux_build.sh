7af0abfb8f09be6ec3247865f0f6344b28d2bb1d
Will Ayd <william.ayd@icloud.com>
2020-02-12 09:11:13 -0800

    First attempt

---
+++ manylinux_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+# Some paths require the period, others dont
+PYVER2="${PYVER//.}"
+
+# Starting in Python38 the ABI version is no longer required
+if [ "$PYVER2" = "37" ] || [ "$PYVER2" = "36" ]
+then
+    ABIVER="m"
+else
+    ABIVER=""
+fi
+
+PYLOC=/opt/python/cp${PYVER2}-cp${PYVER2}${ABIVER}
+
+${PYLOC}/bin/python -m pip install --upgrade pip setuptools wheel auditwheel
+
+cd /io
+${PYLOC}/bin/python setup.py bdist_wheel
+# TODO: we can be more prescriptive about the wheel being repaired
+for whl in dist/pantab*.whl; do
+    ${PYLOC}/bin/python -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
