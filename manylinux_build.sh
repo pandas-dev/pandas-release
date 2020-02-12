e244f6852d31d5c43af149fa1182db9d652c66a6
Will Ayd <william.ayd@icloud.com>
2020-02-12 09:53:31 -0800

    Added testing requirements

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -19,6 +19,6 @@ ${PYLOC}/bin/python -m pip install cython numpy pytz python-dateutil
 cd /io
 ${PYLOC}/bin/python setup.py bdist_wheel
 # TODO: we can be more prescriptive about the wheel being repaired
-for whl in dist/pantab*.whl; do
+for whl in dist/pandas*.whl; do
     ${PYLOC}/bin/python -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
