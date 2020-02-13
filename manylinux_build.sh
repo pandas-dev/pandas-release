62fc6604d75874da455320433e1cdf56f32a1008
Will Ayd <william.ayd@icloud.com>
2020-02-12 21:36:24 -0800

    build updates

--- manylinux_build.sh
+++ manylinux_build.sh
@@ -17,6 +17,7 @@ ${PYLOC}/bin/python -m pip install --upgrade pip setuptools wheel auditwheel
 ${PYLOC}/bin/python -m pip install cython numpy
 
 cd /io
+${PYLOC}/bin/python setup.py build_ext --inplace -j 4
 ${PYLOC}/bin/python setup.py bdist_wheel
 # TODO: we can be more prescriptive about the wheel being repaired
 for whl in dist/pandas*.whl; do
